6b913cb4edf4a76d69f441a6f5fb3c045b2010e0
munir <munirabughannam@gmail.com>
2023-02-04 15:57:48 -0800

    added ordering fields and search fields

--- CryptoPI/api/views.py
+++ CryptoPI/api/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
 from rest_framework import generics
-from django.http import response
 from .models import crypto
 from .serializers import cryptoserializer
+
+
 
 class CoinView(generics.ListAPIView):
     queryset = crypto.objects.all()
     serializer_class = cryptoserializer
+    ordering_fields = ['price']
+    search_fields = ['coin_name']
 
 
 
